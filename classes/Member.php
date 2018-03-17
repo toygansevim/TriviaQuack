5fce1d4863b8763256e5fc7e847646301f7f7468
Mason Hernandez <mhernandez58@mail.greenriver.edu>
2018-03-17 17:27:48 -0400

    Added fields

--- classes/Member.php
+++ classes/Member.php
@@ -13,15 +13,17 @@
 
 class Member extends Player
 {
-    protected $email, $dateJoined, $id;
+    protected $email, $dateJoined, $id, $totalPlayed, $categoryCounts;
     protected $friends = array();
 
-    public function __construct($id, $userName, $email, $dateJoined, $score)
+    public function __construct($id, $userName, $email, $dateJoined, $score, $totalPlayed, $categoryCounts)
     {
         parent::__construct($userName, $score);
         $this->dateJoined = $dateJoined;
         $this->email = $email;
         $this->id = $id;
+        $this->totalPlayed = $totalPlayed;
+        $this->categoryCounts = $categoryCounts;
     }
 
     //********************************************
@@ -38,6 +40,40 @@ class Member extends Player
     {
         return $this->email;
     }
+
+
+    /**
+     * @return mixed
+     */
+    public function getTotalPlayed()
+    {
+        return $this->totalPlayed;
+    }
+
+    /**
+     * @param mixed $totalPlayed
+     */
+    public function setTotalPlayed($totalPlayed)
+    {
+        $this->totalPlayed = $totalPlayed;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCategoryCounts()
+    {
+        return $this->categoryCounts;
+    }
+
+    /**
+     * @param mixed $categoryCounts
+     */
+    public function setCategoryCounts($categoryCounts)
+    {
+        $this->categoryCounts = $categoryCounts;
+    }
+
 
     /**
      * @param mixed $email
